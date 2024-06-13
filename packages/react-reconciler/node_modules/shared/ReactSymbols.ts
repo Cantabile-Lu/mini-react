335af1e6bc92fbd6587405b899effdf88147528a
luwenhai99999 <834299862@qq.com>
2024-06-13 19:07:16 +0800

    feat: update

--- packages/react-reconciler/node_modules/shared/ReactSymbols.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description 当前环境是否支持Symbol
- * @date: 2023-08-16
- */
-const supportsSymbol = typeof Symbol === 'function' && Symbol.for;
-
-export const REACT_ELEMENT_TYPE = supportsSymbol
-	? Symbol.for('react.element')
-	: 0xeac7;
